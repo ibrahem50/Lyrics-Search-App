e1802473922f2f3966f33901bed2e0e658bf38eb
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-01-13 07:12:14 +0200

    refactored app.js using react hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router , Route , Switch} from 'react-router-dom'
-import Navbar from './components/layout/Navbar';
-import Index from './components/layout/Index';
-import { Provider } from './components/context';
-import Lyrics from './components/tracks/Lyrics';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
 
+import "./App.css";
 
-class App extends Component {
-  state = {  }
-  render() { 
-    return ( 
-     
-     <Provider>
+import { ContextController } from "./context";
+
+const App = () => {
+  return (
+    <ContextController>
       <Router>
-       <React.Fragment>
-         <Navbar/>
-         <div className="container">
-           <Switch>
-             <Route exact path='/' component={Index}/>
-             <Route exact path ='/lyrics/trak/:id' component={Lyrics}/>
-           </Switch>
-         </div>
-       </React.Fragment>
+        <>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
+            </Switch>
+          </div>
+        </>
       </Router>
-    </Provider>
-     );
-  }
-}
- 
+    </ContextController>
+  );
+};
+
 export default App;
