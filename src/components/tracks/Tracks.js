25109ff6514f60baec503a681dbdc32784c70cd5
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-01-13 04:51:29 +0200

    added Tracks component

---
+++ src/components/tracks/Tracks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class Tracks extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Tracks</h1>
+            </div>
+        );
+    }
+}
+ 
+export default Tracks;
