25407c9201bc6e4667290ce46070ed14002cf221
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-01-13 06:54:08 +0200

    added lyrics component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router , Route , Switch} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
 import { Provider } from './components/context';
+import Lyrics from './components/tracks/Lyrics';
 
 
 
@@ -18,6 +19,7 @@ class App extends Component {
          <div className="container">
            <Switch>
              <Route exact path='/' component={Index}/>
+             <Route exact path ='/lyrics/trak/:id' component={Lyrics}/>
            </Switch>
          </div>
        </React.Fragment>
