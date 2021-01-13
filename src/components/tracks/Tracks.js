2f25c884f80cb2fe841287b9522746cf898114a2
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-01-13 07:17:43 +0200

    refactored tracks component using react hooks

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,37 +1,26 @@
-import React, { Component } from 'react';
-import { Consumer } from '../context';
-import Spinner from '../layout/Spinner';
-import Track from './Track';
+import React, { useContext } from "react";
+import { Context } from "../../context";
+import Spinner from "../layout/Spinner";
+import Track from "../tracks/Track";
 
-class Tracks extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                <Consumer>
-                    {value =>{
-                        const {track_list, heading} = value;
-                        if (track_list.length===0||track_list===undefined){
-                          return <Spinner/>
-                        } else {
-                          return (
-                              <React.Fragment>
-                                  <h3 className="text-center mb-4">{heading}</h3>
-                                  <div className="row">
-                                      {track_list.map(item=>{
-                                          return(
-                                          <Track key={item.track.track_id} track={item.track}/>
-                                          )
-                                      })}
-                                  </div>
-                              </React.Fragment>
-                          )
-                        }
-                    }}
-                </Consumer>
-            </div>
-        );
-    }
-}
- 
+const Tracks = () => {
+  const [state] = useContext(Context);
+  const { track_list, heading } = state;
+
+  if (track_list === undefined || track_list.length === 0) {
+    return <Spinner />;
+  } else {
+    return (
+      <>
+        <h3 className="text-center mb-4">{heading}</h3>
+        <div className="row">
+          {track_list.map(item => (
+            <Track key={item.track.track_id} track={item.track} />
+          ))}
+        </div>
+      </>
+    );
+  }
+};
+
 export default Tracks;
