c3b1bf27aee52e5f4abe54cd3c11d80d5561241f
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-01-13 03:31:16 +0200

    added navbar component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        <Navbar/>
+      </div>
+     );
+  }
+}
+ 
+export default App;
