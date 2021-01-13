61c6c49a8bbab057fa09e9340a42942505f4c3e2
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-01-13 06:09:48 +0200

    added trak component

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Consumer } from '../context';
 import Spinner from '../layout/Spinner';
+import Track from './Track';
 
 class Tracks extends Component {
     state = {  }
@@ -9,11 +10,22 @@ class Tracks extends Component {
             <div>
                 <Consumer>
                     {value =>{
-                        const {track_list} = value;
+                        const {track_list, heading} = value;
                         if (track_list.length===0||track_list===undefined){
                           return <Spinner/>
                         } else {
-                          return <h1>Tracks loaded</h1>
+                          return (
+                              <React.Fragment>
+                                  <h3 className="text-center mb-4">{heading}</h3>
+                                  <div className="row">
+                                      {track_list.map(item=>{
+                                          return(
+                                          <Track key={item.track.track_id} track={item.track}/>
+                                          )
+                                      })}
+                                  </div>
+                              </React.Fragment>
+                          )
                         }
                     }}
                 </Consumer>
