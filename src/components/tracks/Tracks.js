e822fb976959a5686a0f2e275b105029f932a5e6
ibrahem <ibrahem.ahmed.ibrahem500@gmail.com>
2021-01-13 05:54:19 +0200

    added spinner

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Consumer } from '../context';
+import Spinner from '../layout/Spinner';
 
 class Tracks extends Component {
     state = {  }
@@ -8,8 +9,12 @@ class Tracks extends Component {
             <div>
                 <Consumer>
                     {value =>{
-                        console.log(value);
-                        return <h1>Tracks</h1>
+                        const {track_list} = value;
+                        if (track_list.length===0||track_list===undefined){
+                          return <Spinner/>
+                        } else {
+                          return <h1>Tracks loaded</h1>
+                        }
                     }}
                 </Consumer>
             </div>
